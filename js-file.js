0decb37384dcb24b59c5293ee4d5c82c1e03436a
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-12 10:30:16 +1000

    Create boxes inside the main container using  flexbox
    16*16 boxes were created and a small gap was created in between them using flexbox properties.

--- js-file.js
+++ js-file.js
@@ -1,5 +1,27 @@
 function selectColor() {
-    colors = ["red", "blue", "yellow", "navy", "orange", "green", "aqua"];
-    let a = Math.floor(Math.random() * 7);
-    return colors[a];
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    return "rgb("+r+","+g+","+b+")";
 }
+
+let container = document.querySelector(".container");
+function createBoxes(){
+    for (let i = 0; i < 16; i++){
+        for (let j = 0; j < 16; j++){
+            let div = document.createElement("div");
+            div.classList.add("individual");
+            //div.style.width = "25px";
+            //div.style.height = "25px";
+            div.style.backgroundColor = selectColor();
+            container.appendChild(div);
+            
+        }
+    }
+}
+
+createBoxes();
