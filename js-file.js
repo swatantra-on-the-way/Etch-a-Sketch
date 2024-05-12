1c587edb7b6598b0285c4b2398281688b05eda12
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-05-12 13:56:29 +1000

    Add functionality to Grid Size button to fill grid
    After the Grid Size button is clicked, it deletes the previous grids and updates the container with new grids, whose value is entered by the user.

--- js-file.js
+++ js-file.js
@@ -1,3 +1,27 @@
+let a = document.querySelector("#grid-size");
+a.addEventListener("click", function setGridSize(e){
+    let a = prompt("Enter the Grid size you want (1 - 100): ");
+    if (parseInt(a) >= 0 && parseInt(a) <= 100) {
+        grid_size = parseInt(a);
+        deleteGrid(grid_size);
+        createGrid(grid_size);
+    }
+});
+
+// function clearGrid() {
+//     let all = document.querySelectorAll(".individual");
+//     for (let i = 0; i < all.length; i++){
+//         all[i].style.backgroundColor = "white";
+//     }
+// }
+
+function deleteGrid(grid_size){
+    let parent = document.querySelector(".container");
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 function selectColor() {
     let r = 0;
     let g = 0;
@@ -10,11 +34,13 @@ function selectColor() {
 }
 
 let container = document.querySelector(".container");
-function createBoxes(){
-    for (let i = 0; i < 16; i++){
-        for (let j = 0; j < 16; j++){
+function createGrid(grid_size){
+    for (let i = 0; i < grid_size; i++){
+        for (let j = 0; j < grid_size; j++){
             let div = document.createElement("div");
             div.classList.add("individual");
+            div.style.setProperty("--columns", grid_size);
+            div.style.setProperty("--rows", grid_size);
             
             div.addEventListener("mouseover", () => {
                 div.style.backgroundColor = selectColor();
@@ -23,5 +49,6 @@ function createBoxes(){
         }
     }
 }
+let grid_size = 16;
+createGrid(grid_size);
 
-createBoxes();
